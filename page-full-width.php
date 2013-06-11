aa84c7f48404f34b1cf87d30473b11ca9c5ca1a4
Zoe Rooney <hello@zoerooney.com>
2013-06-11 07:37:59 -0400

    Full Width Page & Clearfix
    Added full width page template.
    Added clearfix to reset stylesheet.

---
+++ page-full-width.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="full-width">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
