c35a655d9a1408a1b5bbe74897a06038da4cdd74
Zoe Rooney <zoe@zoerooney.com>
2013-09-20 07:00:44 -0400

    Added Redirect to URL template
    Requires ACF plugin/ setup, otherwise delete on launch
    
    Signed-off-by: Zoe Rooney <zoe@zoerooney.com>

---
+++ page-redirect-url.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Redirect To URL
+*/
+if (have_posts() {
+  while (have_posts()) {
+    the_post();
+    if ( get_field('url') ) :
+    $redirecturl = get_field('url');
+    wp_redirect($redirecturl);
+    exit;
+    else :
+    echo 'Your theme is not set up to use this template, or you have not specified a URL.';
+    endif;
+  }
+}
+?>
