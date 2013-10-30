7fe298636beb74f30fb8f4d13f5e21093f91ad5f
Zoe Rooney <zoe@zoerooney.com>
2013-10-30 15:15:53 -0400

    Content Tweaks
    Added space between year and name; added designer credit; reformatted ZRWD credit per legal
    
    Signed-off-by: Zoe Rooney <zoe@zoerooney.com>

--- footer.php
+++ footer.php
@@ -14,7 +14,8 @@
 
 	<footer id="colophon" role="contentinfo">
 			<div id="site-generator">
-				&copy; <?php echo date('Y'); echo bloginfo( 'name' ); ?><a href="http://zoerooney.com" target="_blank">Development by Zoe Rooney</a>
+				&copy; <?php echo date('Y'); echo '&nbsp;'; echo bloginfo( 'name' ); ?><br>
+				Site by <a href="http://" target="_blank">DESIGNER</a> &amp; <a href="http://zoerooney.com" target="_blank">Development by Zoe Rooney</a>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
