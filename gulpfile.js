f3ae6ce631e079090ef55053bf2daab008ec0fb6
Zoe Rooney <zoe@zoerooney.com>
2014-04-23 18:24:22 -0400

    further simplified

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,6 @@ gulp.task('init', function(){
 });
 gulp.task('commit', function(){
 	return gulp.src('./*')
-	.pipe(git.add())
 	.pipe(git.commit('initial commit', {args: '-a'}));
 });
 gulp.task('setup',['styles','init','commit']);
