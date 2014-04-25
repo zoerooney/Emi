c314014db1dc85be1bea38f6d36661891c5883f9
Zoe Rooney <hello@zoerooney.com>
2014-04-25 09:15:19 -0400

    Updated TinyMCE custom colors snippet, break functions into new module, fixes #28

--- functions.php
+++ functions.php
@@ -86,46 +86,6 @@ add_action('wp_enqueue_scripts', 'themeFunction_scripts_method');
 // Comments & pingbacks display template
 include('inc/functions/comments.php');
 
-// Remove admin bar for all users
-show_admin_bar( false );
-
-// Move ACF Options menu under Settings
-//if( function_exists('acf_add_options_sub_page') )
-//{
-//    acf_add_options_sub_page(array(
-//        'title' => __( 'Site Options', 'themeTextDomain' ),
-//        'parent' => 'options-general.php',
-//        'capability' => 'manage_options'
-//    ));
-//}
-
-// Add TinyMCE buttons that are disabled by default
-//function themeFunction_mce_buttons_2($buttons) {	
-//	/**
-//	 * Add in a core button that's disabled by default
-//	 */
-//	$buttons[] = 'justify'; // fully justify text
-//	$buttons[] = 'hr'; // insert HR
-//
-//	return $buttons;
-//}
-//add_filter('mce_buttons_2', 'themeFunction_mce_buttons_2');
-
-
-// Remove all colors except those custom colors specified from TinyMCE
-//function themeFunction_change_mce_options( $init ) {
-//	$init['theme_advanced_text_colors'] = '8dc63f';
-//	$init['theme_advanced_more_colors'] = false;
-//return $init;
-//}
-//add_filter('tiny_mce_before_init', 'themeFunction_change_mce_options');
-
-
-// Modify the query on a given template (using conditionals)
-//function themeFunction_custom_query($query) {
-//    if ($query->is_search) {
-//        $query->set('post_type', 'post');
-//    }
-//    return $query;
-//}
-//add_filter('pre_get_posts','themeFunction_custom_query');
+// Optional Customizations
+// Includes: TinyMCE tweaks, admin menu & bar settings, query overrides
+include('inc/functions/customizations.php');
---
+++ inc/functions/customizations.php
@@ -0,0 +1,45 @@
+<?php 
+// Remove admin bar for all users
+show_admin_bar( false );
+
+// Move ACF Options menu under Settings
+//if( function_exists('acf_add_options_sub_page') )
+//{
+//    acf_add_options_sub_page(array(
+//        'title' => __( 'Site Options', 'themeTextDomain' ),
+//        'parent' => 'options-general.php',
+//        'capability' => 'manage_options'
+//    ));
+//}
+
+// Add TinyMCE buttons that are disabled by default
+//function themeFunction_mce_buttons_2($buttons) {	
+//	/**
+//	 * Add in a core button that's disabled by default
+//	 */
+//	$buttons[] = 'justify'; // fully justify text
+//	$buttons[] = 'hr'; // insert HR
+//
+//	return $buttons;
+//}
+//add_filter('mce_buttons_2', 'themeFunction_mce_buttons_2');
+
+
+// Remove all colors except those custom colors specified from TinyMCE
+//function themeFunction_change_mce_options( $init ) {
+//	$custom_colors = '"#####1", "Color Name 1", "#####2", "Color Name 2", "#####3", "Color Name 3"';	
+//	$init['textcolor_map'] = '['.$custom_colors.']';
+//return $init;
+//}
+//add_filter('tiny_mce_before_init', 'themeFunction_change_mce_options');
+
+
+// Modify the query on a given template (using conditionals)
+//function themeFunction_custom_query($query) {
+//    if ($query->is_search) {
+//        $query->set('post_type', 'post');
+//    }
+//    return $query;
+//}
+//add_filter('pre_get_posts','themeFunction_custom_query');
+?>
