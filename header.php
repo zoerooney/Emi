855bb297073cafd08d13c5fdd6c47f0d1b8d12f9
Zoe Rooney <hello@zoerooney.com>
2014-09-18 07:49:22 -0400

    add initial scale to meta viewport tag

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     
